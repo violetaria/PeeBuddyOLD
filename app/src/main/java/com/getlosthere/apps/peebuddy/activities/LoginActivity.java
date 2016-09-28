dd69dca7f4dce22d8a084ba695d730e9aa62bce5
violetaria <terri.chu@outlook.com>
2016-09-27 22:22:27 -0400

    fb login working, initial call to rails working

--- app/src/main/java/com/getlosthere/apps/peebuddy/activities/LoginActivity.java
+++ app/src/main/java/com/getlosthere/apps/peebuddy/activities/LoginActivity.java
@@ -15,23 +15,47 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.getlosthere.apps.peebuddy.R;
 import com.getlosthere.apps.peebuddy.api_clients.FacebookClient;
+import com.getlosthere.apps.peebuddy.api_clients.PeeBuddyClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
+import cz.msebera.android.httpclient.Header;
+
 public class LoginActivity extends OAuthLoginActionBarActivity<FacebookClient> {
 	private CallbackManager callbackManager;
+	PeeBuddyClient peeBuddyClient;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		callbackManager = CallbackManager.Factory.create();
+		peeBuddyClient = new PeeBuddyClient();
 
 		LoginManager.getInstance().registerCallback(callbackManager,
 				new FacebookCallback<LoginResult>() {
 					@Override
 					public void onSuccess(LoginResult loginResult) {
 						Toast.makeText(getApplicationContext(),"login succeeded",Toast.LENGTH_LONG).show();
+						Log.d("DEBUG",loginResult.getAccessToken().toString());
+						String fb_token = loginResult.getAccessToken().getToken().toString();
+                        String fb_user_id = loginResult.getAccessToken().getUserId().toString();
+						String expires_at = loginResult.getAccessToken().getExpires().toString();
+						peeBuddyClient.createSession(fb_token, fb_user_id, expires_at, new JsonHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers,
+                                    JSONObject response) {
+                                super.onSuccess(statusCode, headers, response);
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                                    Log.e("ERROR",errorResponse.toString());
+                            }
+                        });
 					}
 
 					@Override
@@ -64,6 +88,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<FacebookClient> {
 	// i.e Display application "homepage"
 	@Override
 	public void onLoginSuccess() {
+
 		// Intent i = new Intent(this, PhotosActivity.class);
 		// startActivity(i);
 	}
@@ -79,8 +104,7 @@ public class LoginActivity extends OAuthLoginActionBarActivity<FacebookClient> {
 	// Uses the client to initiate OAuth authorization
 	// This should be tied to a button used to login
 	public void loginToFacebook(View view) {
-		LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
-
+		LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile","email"));
 	}
 
 }
--- app/src/main/java/com/getlosthere/apps/peebuddy/api_clients/PeeBuddyClient.java
+++ app/src/main/java/com/getlosthere/apps/peebuddy/api_clients/PeeBuddyClient.java
@@ -1,23 +1,46 @@
 package com.getlosthere.apps.peebuddy.api_clients;
 
+import android.util.Log;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PeeBuddyClient {
-	private static final String BASE_URL = "http://localhost:3000/api/v1";
+	private static final String BASE_URL = "https://9f3f2026.ngrok.io";
 
 	private static AsyncHttpClient client = new AsyncHttpClient();
 
-	// https://localhost:3000/api/v1/users
-	public static void login(String username, String password, AsyncHttpResponseHandler responseHandler) {
-		String url = "/users/";
-		RequestParams loginParams = new RequestParams();
-		loginParams.add("user",username);
-		loginParams.add("password",password);
-		RequestParams params = new RequestParams();
-		params.add("user",loginParams.toString());
+//	// https://localhost:3000/api/v1/users
+//	public static void login(String username, String password, AsyncHttpResponseHandler responseHandler) {
+//		String url = "/users/";
+//		RequestParams loginParams = new RequestParams();
+//		loginParams.add("user",username);
+//		loginParams.add("password",password);
+//		RequestParams params = new RequestParams();
+//		params.add("user",loginParams.toString());
+//
+//		client.post(getAbsoluteUrl(url), params, responseHandler);
+//	}
 
+	// https://localhost:3000/auth/facebook
+	public static void facebookLogin(AsyncHttpResponseHandler responseHandler) {
+		String url = "/auth/facebook/";
+		Log.d("DEBUG",getAbsoluteUrl(url));
+		client.get(getAbsoluteUrl(url), null, responseHandler);
+	}
+
+	public static void createSession(String token, String user_id, String expires_at, AsyncHttpResponseHandler responseHandler) {
+		String url = "/sessions/";
+		RequestParams params = new RequestParams();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("token", token);
+		map.put("user_id",user_id);
+		map.put("expires_at",expires_at);
+		params.put("session", map);
 		client.post(getAbsoluteUrl(url), params, responseHandler);
 	}
 
